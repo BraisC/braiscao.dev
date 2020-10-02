48fd3540963cbe1f779181befeb643f6c693922d
Brais <Brais@MacOS.local>
2020-10-02 12:48:55 +0200

    🧼 Cleanup default files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import Image from '../components/Image';
 import SEO from '../components/SEO';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Hi, my name is Brais</h1>
+    <p>I am a web developer</p>
   </Layout>
 );
 
