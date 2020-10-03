84567766cb69c3a293095b82787428ac3ac8f0ac
Brais <Brais@MacOS.local>
2020-10-03 12:40:58 +0200

    🔥 wrapPageElement also in ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react';
+import Layout from './src/components/Layout';
+
+export const wrapPageElement = ({ element, props }) => <Layout {...props}>{element}</Layout>;
