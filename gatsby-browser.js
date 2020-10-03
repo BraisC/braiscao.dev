4bf0943a84dd8a3375582f9f898e2691bad3ef93
Brais <Brais@MacOS.local>
2020-10-03 12:37:40 +0200

    🔥 Layout component is now inside wrapPageElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 import 'typeface-montserrat';
+import React from 'react';
+import Layout from './src/components/Layout';
+
+export const wrapPageElement = ({ element, props }) => <Layout {...props}>{element}</Layout>;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-
-import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <h1>Hi, my name is Brais</h1>
     <p>I am a web developer</p>
-  </Layout>
+  </>
 );
 
 export default IndexPage;
