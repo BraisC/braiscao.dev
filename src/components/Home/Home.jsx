655978ab330c832433bae542e3d99f19ffb9bc15
Brais <Brais@MacOS.local>
2020-10-04 18:11:27 +0200

    📚 Home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import Typer from './components/Typer';
+
+const StyledHeader = styled.h1`
+  font-size: 9.6rem;
+  font-weight: 700;
+`;
+
+const StyledName = styled.span`
+  color: var(--color-primary);
+  display: block;
+`;
+
+const StyledSubTitle = styled.p`
+  color: var(--color-white);
+  font-size: 6.4rem;
+  font-weight: 700;
+  margin-top: 1.6rem;
+`;
+const StyledTyper = styled(Typer)`
+  color: var(--color-primary);
+  font-size: 6.4rem;
+  font-weight: 700;
+`;
+
+const Home = (props) => (
+  <>
+    <StyledHeader>
+      Hi, my name is <StyledName>Brais</StyledName>
+    </StyledHeader>
+    <StyledSubTitle>
+      I am{' '}
+      <StyledTyper
+        cursorColor="#71e2a6"
+        pauseTimer={1000}
+        dataText={['a web developer.', 'from Spain.', 'passionate.', 'a geek.']}
+      />
+    </StyledSubTitle>
+  </>
+);
+
+export default Home;
