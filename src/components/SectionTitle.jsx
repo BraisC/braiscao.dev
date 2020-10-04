74822561348c99e7fb8f9b765c5ee958e2d359ff
Brais <Brais@MacOS.local>
2020-10-04 19:38:41 +0200

    🔥 Section title component

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  color: var(--color-primary);
+  font-size: 3rem;
+  text-transform: uppercase;
+  font-weight: 700;
+`;
+const SubTitle = styled.h3`
+  color: var(--color-white);
+  font-size: 1.4rem;
+  font-weight: 500;
+`;
+
+const SectionTitle = ({ title, subTitle }) => (
+  <>
+    <Title>{title}</Title>
+    <SubTitle>{subTitle}</SubTitle>
+  </>
+);
+
+export default SectionTitle;
