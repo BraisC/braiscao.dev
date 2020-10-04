a20e406525294e6618bb0416df83d1e6ddcab94e
Brais <Brais@MacOS.local>
2020-10-04 19:00:02 +0200

    🔥 Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.a`
+  border: 1px solid var(--color-primary);
+  color: var(--color-white);
+  padding: 1rem 2rem;
+  font-size: 1.6rem;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: #71e2a662;
+  }
+`;
+
+const Button = ({ children }) => <StyledButton>{children}</StyledButton>;
+
+export default Button;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from '../Button';
 import Typer from './components/Typer';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 12rem;
+  margin-bottom: 12rem;
+`;
 
 const StyledHeader = styled.h1`
   font-size: 9.6rem;
   font-weight: 700;
+  width: 100%;
 `;
 
 const StyledName = styled.span`
@@ -17,6 +28,8 @@ const StyledSubTitle = styled.p`
   font-size: 6.4rem;
   font-weight: 700;
   margin-top: 1.6rem;
+  margin-bottom: 12rem;
+  width: 100%;
 `;
 const StyledTyper = styled(Typer)`
   color: var(--color-primary);
@@ -25,7 +38,7 @@ const StyledTyper = styled(Typer)`
 `;
 
 const Home = (props) => (
-  <>
+  <Wrapper>
     <StyledHeader>
       Hi, my name is <StyledName>Brais</StyledName>
     </StyledHeader>
@@ -37,7 +50,9 @@ const Home = (props) => (
         dataText={['a web developer.', 'from Spain.', 'passionate.', 'a geek.']}
       />
     </StyledSubTitle>
-  </>
+
+    <Button>Let me show you my work</Button>
+  </Wrapper>
 );
 
 export default Home;
