4dab4d6f7a091a740eef45b9b8f882b84fbacddb
Brais <braiscao.dev@gmail.com>
2020-10-05 20:07:04 +0200

    🎠 Overlay animated with Framer Motion

--- src/components/Portfolio/components/Item.jsx
+++ src/components/Portfolio/components/Item.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
 import Button from '../../Button';
 
 const Wrapper = styled.div`
   width: 33%;
   position: relative;
+  overflow: hidden;
 `;
 
 const Image = styled(Img)`
   width: 100%;
 `;
 
-const Overlay = styled.article`
+const Overlay = styled(motion.article)`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -25,12 +27,12 @@ const Overlay = styled.article`
   background-color: #0a2027d8;
 `;
 
-const TitleWrapper = styled.div`
+const TitleWrapper = styled(motion.div)`
   text-transform: uppercase;
   text-align: center;
 `;
 
-const Title = styled.h1`
+const Title = styled(motion.h1)`
   font-size: 2rem;
   font-weight: 700;
   margin-bottom: 0.4rem;
@@ -47,18 +49,65 @@ const StyledButton = styled(Button)`
   font-size: 1.4rem;
 `;
 
+const overlayVariants = {
+  open: {
+    y: '0',
+    transition: {
+      duration: 0.2,
+      when: 'beforeChildren',
+      staggerChildren: 0.1,
+      bounce: 0,
+    },
+  },
+  close: {
+    y: '100%',
+    transition: {
+      duration: 0.2,
+      when: 'afterChildren',
+      staggerChildren: 0.1,
+      bounce: 0,
+    },
+  },
+};
+
+const childVariants = {
+  open: {
+    x: '0',
+  },
+  close: {
+    x: '200%',
+    transition: {
+      duration: 0.1,
+      bounce: 0,
+    },
+  },
+};
+
 const Item = ({ data }) => {
-  console.log(data);
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
-    <Wrapper>
+    <Wrapper onMouseEnter={() => setIsVisible(true)} onMouseLeave={() => setIsVisible(false)}>
       <Image fluid={data.image.childImageSharp.fluid} />
-      <Overlay>
-        <TitleWrapper>
-          <Title>{data.title}</Title>
-          <SubTitle>{data.stack}</SubTitle>
-        </TitleWrapper>
-        <StyledButton>More info</StyledButton>
-      </Overlay>
+      <AnimatePresence>
+        {isVisible && (
+          <Overlay
+            key={data.title}
+            initial="close"
+            animate="open"
+            exit="close"
+            variants={overlayVariants}
+          >
+            <TitleWrapper variants={childVariants}>
+              <Title>{data.title}</Title>
+              <SubTitle>{data.stack}</SubTitle>
+            </TitleWrapper>
+            <motion.div variants={childVariants}>
+              <StyledButton>More info</StyledButton>
+            </motion.div>
+          </Overlay>
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 };
