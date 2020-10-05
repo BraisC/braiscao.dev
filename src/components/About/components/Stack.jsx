3925c847976908499e39e9231ce9c495c15a1152
Brais <braiscao.dev@gmail.com>
2020-10-05 22:01:42 +0200

    🔥 Added stack to about section

---
+++ src/components/About/components/Stack.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StackText = styled.p`
+  font-size: 1.6rem;
+  font-weight: 500;
+  text-align: center;
+  width: 80%;
+  margin-bottom: 2rem;
+
+  span {
+    font-weight: 700;
+  }
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+
+  &:last-of-type {
+    margin-top: 2rem;
+    margin-bottom: 3rem;
+  }
+`;
+
+const Stack = ({ children }) => <StackText>{children}</StackText>;
+
+export default Stack;
