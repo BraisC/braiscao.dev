e74d20cc1f535d5b06013e0cc4e30f4ff04abe65
Brais <braiscao.dev@gmail.com>
2020-10-05 20:32:20 +0200

    🔥 Created Contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Section from '../Section';
+
+const Contact = () => <Section title="contact" subTitle="You can find me at this places" />;
+export default Contact;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import Portfolio from '../components/Portfolio/Portfolio';
 import About from '../components/About/About';
 
 import SEO from '../components/SEO';
+import Contact from '../components/Contact/Contact';
 
 const IndexPage = () => (
   <>
@@ -11,6 +12,7 @@ const IndexPage = () => (
     <Home />
     <Portfolio />
     <About />
+    <Contact />
   </>
 );
 export default IndexPage;
