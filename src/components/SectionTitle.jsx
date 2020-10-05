6fcf0eb5439da52f52ea9fd8598f5dae56ffe6f2
Brais <braiscao.dev@gmail.com>
2020-10-05 22:20:09 +0200

    👀 Less margin

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -11,7 +11,7 @@ const SubTitle = styled.h3`
   color: var(--color-white);
   font-size: 1.4rem;
   font-weight: 500;
-  margin-bottom: 8rem;
+  margin-bottom: 6rem;
 `;
 
 const SectionTitle = ({ title, subTitle }) => (
