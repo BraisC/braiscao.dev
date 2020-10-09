f4b8da12bcbfd58a6f49493010dfec49411b0c46
Brais <braiscao.dev@gmail.com>
2020-10-09 14:07:29 +0200

    🔥 Sharing image and fix on 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,7 @@ import SEO from '../components/SEO';
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
+    <h1 style={{ paddingTop: '10rem' }}>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </>
 );
